017ca3463f15dd4a7110bff59f63b8ae72112eb0
adil <adil@leadiq.com>
2021-09-26 10:49:26 +0500

    fix helm chart path

--- argocd_create_app.sh
+++ argocd_create_app.sh
@@ -1,1 +1,1 @@
-argocd app create demo-app --repo https://github.com/Adiii717/argocd-demo-app.git --path helm-guestbook --dest-namespace default --dest-server https://kubernetes.default.svc --helm-set replicaCount=2
+argocd app create demo-app --repo https://github.com/Adiii717/argocd-demo-app.git --path helm-chart --dest-namespace default --dest-server https://kubernetes.default.svc --helm-set replicaCount=2
